d5612928d5c3544152f23f9ae77247d9dcfff020
ivancraze <ivancraze@yandex.ru>
2020-10-04 23:15:12 +0400

    add errors catch and spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-ripple-hkkmnhicj0k">
+            <div className="ldio-6hf3qgreiib">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
